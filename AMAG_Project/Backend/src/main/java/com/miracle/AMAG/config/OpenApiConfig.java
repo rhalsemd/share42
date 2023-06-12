9bfa5021efea6fa7ce9d8072a0c7f7a72a4f62df
rhalsemd <rhalsemd@naver.com>
2023-06-12 16:54:27 +0900

    노션 링크 및 이메일 주소 삭제

--- AMAG_Project/Backend/src/main/java/com/miracle/AMAG/config/OpenApiConfig.java
+++ AMAG_Project/Backend/src/main/java/com/miracle/AMAG/config/OpenApiConfig.java
@@ -18,7 +18,6 @@ public class OpenApiConfig {
         Info info = new Info().title("공유사이 API")
                 .description("공유사이 웹 애플리케이션 Rest API 명세서 입니다.")
                 .version("v0.0.1")
-                .contact(new Contact().name("share-42-notion").url("https://www.notion.so/miracle3070/SSAFY-8-D102-6b934c1f09e846b8b2f2ae6fcf4d15a1").email("ssafy.miracle3070@gmail.com"))
                 .license(new License().name("license 추가 예정").url(""));
 
         SecurityScheme basicAuth = new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer");
